b93988fa7c1785adcf99f10e2f2fd4fdbb66032f
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-08-07 11:27:53 -0400

    initial page setup

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+    window.pAsyncInit = function() {
+        PDK.init({
+            appId: "5046572227298896564", // Change this
+            cookie: true
+        });
+    };
+
+    (function(d, s, id){
+        var js, pjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//assets.pinterest.com/sdk/sdk.js";
+        pjs.parentNode.insertBefore(js, pjs);
+    }(document, 'script', 'pinterest-jssdk'));
